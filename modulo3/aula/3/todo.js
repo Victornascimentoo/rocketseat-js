89e0ab77e7be31e251c5638256cf4aab034652b6
unknown <victor_nascimento2@saojose.sc.senai.br>
2019-06-17 12:31:04 -0300

    Iniciando a3 m3

---
+++ modulo3/aula/3/todo.js
@@ -0,0 +1,16 @@
+var listElement = 
+document.querySelector("#app ul");
+var inputElement =
+document.querySelector("#app input");
+var buttonElement = 
+document.querySelector("#app button");
+
+console.log(listElement)
+console.log(inputElement)
+console.log(buttonElement)
+
+var todos = [
+    "fazer cafe",
+    "estudar qualquer coisa",
+    "jogar lolzin"
+];
